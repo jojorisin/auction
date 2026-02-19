fbefc96cc883307599ee727ebd904f96bbb6a687
johannajonsson <jojo.risin@outlook.com>
2026-02-19 14:16:21 +0100

    update getMyActiveBids to show bidders maxSum

--- src/main/java/se/jensen/johanna/auctionsite/repository/MaxBidRepository.java
+++ src/main/java/se/jensen/johanna/auctionsite/repository/MaxBidRepository.java
@@ -1,9 +1,20 @@
 package se.jensen.johanna.auctionsite.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import se.jensen.johanna.auctionsite.model.MaxBid;
 
+import java.util.List;
+
 @Repository
 public interface MaxBidRepository extends JpaRepository<MaxBid, Long> {
+
+    @Query("SELECT m.auction.id, MAX (m.maxSum) FROM MaxBid m WHERE m.bidder.id=:userId AND m.auction.id IN :auctionIds GROUP BY m.auction.id")
+    List<Object[]> findMaxBidSumByAuctionAndUser_IdIn(
+            @Param("userId") Long userId,
+            @Param("auctionIds") List<Long> auctionIds
+    );
 }
+
