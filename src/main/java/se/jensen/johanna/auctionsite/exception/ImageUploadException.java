f78560d9e7d50f13f3c088858c726c80501352d1
johannajonsson <jojo.risin@outlook.com>
2026-03-26 13:09:50 +0100

    add imageService to compress and upload images to s3

---
+++ src/main/java/se/jensen/johanna/auctionsite/exception/ImageUploadException.java
@@ -0,0 +1,8 @@
+package se.jensen.johanna.auctionsite.exception;
+
+public class ImageUploadException extends DomainException {
+
+  public ImageUploadException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
