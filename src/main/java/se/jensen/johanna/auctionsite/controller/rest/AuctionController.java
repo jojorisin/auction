9b47f38b32aad2523bdf4e6d333ecae1d944832f
johannajonsson <jojo.risin@outlook.com>
2026-04-02 21:09:38 +0200

    update securityconfig, GET max-bid is authenticated, delete unnecessary crossorigin annotation

--- src/main/java/se/jensen/johanna/auctionsite/controller/rest/AuctionController.java
+++ src/main/java/se/jensen/johanna/auctionsite/controller/rest/AuctionController.java
@@ -16,7 +16,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,7 +35,6 @@ import se.jensen.johanna.auctionsite.util.JwtUtils;
 
 @RestController
 @RequestMapping("/api/auctions")
-@CrossOrigin
 @RequiredArgsConstructor
 public class AuctionController {
 
--- src/main/java/se/jensen/johanna/auctionsite/controller/rest/MyController.java
+++ src/main/java/se/jensen/johanna/auctionsite/controller/rest/MyController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -34,7 +33,6 @@ import se.jensen.johanna.auctionsite.util.JwtUtils;
 @PreAuthorize("isAuthenticated()")
 @RestController
 @RequestMapping("/api/me")
-@CrossOrigin
 @RequiredArgsConstructor
 public class MyController {
 
