2a6411653147814a61e9f1f9ebe77204a86399a7
johannajonsson <jojo.risin@outlook.com>
2026-04-03 18:15:34 +0200

    feature scheduled relaunch of not sold auctions, fix email-bug, add asyncExecutor to AsyncConfig

--- src/main/java/se/jensen/johanna/auctionsite/config/AsyncConfig.java
+++ src/main/java/se/jensen/johanna/auctionsite/config/AsyncConfig.java
@@ -1,16 +1,30 @@
 package se.jensen.johanna.auctionsite.config;
 
+import java.util.concurrent.Executor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Slf4j
 @Configuration
 public class AsyncConfig implements AsyncConfigurer {
-    @Override
-    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
-        return ((ex, method, params) ->
-                log.error("Async error in {}: {}", method.getName(), ex.getMessage()));
-    }
+
+  @Override
+  public Executor getAsyncExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(5);
+    executor.setMaxPoolSize(5);
+    executor.setQueueCapacity(500);
+    executor.setThreadNamePrefix("EmailExecutor-");
+    executor.initialize();
+    return executor;
+  }
+
+  @Override
+  public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+    return ((ex, method, params) ->
+        log.error("Async error in {}: {}", method.getName(), ex.getMessage()));
+  }
 }
--- src/main/java/se/jensen/johanna/auctionsite/service/EmailService.java
+++ src/main/java/se/jensen/johanna/auctionsite/service/EmailService.java
@@ -15,71 +15,71 @@ import se.jensen.johanna.auctionsite.dto.EmailRequest;
 @Service
 @RequiredArgsConstructor
 public class EmailService {
-    private final JavaMailSender mailSender;
 
-    @Async
-    public void sendEmail(EmailRequest request) {
-        try {
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-            setEmailContent(helper, request);
-            helper.setTo(request.email());
-            mailSender.send(message);
-        } catch (MessagingException | MailException e) {
-            log.error("Failed to send email to {}: {}", request.email(), e.getMessage());
-        }
-    }
+  private final JavaMailSender mailSender;
 
-    public void setEmailContent(MimeMessageHelper helper, EmailRequest request) throws MessagingException {
-        switch (request.type()) {
-            case WINNER:
-                helper.setSubject(String.format("Congratulations! You won %s.", request.title()));
-                helper.setText(
-                        String.format(
-                                "Congratulations! You won auction %s. %n %s Find payment and transport options under My Wins",
-                                request.title(),
-                                request.imageUrl()
-                        )
-                );
-                break;
-            case OUTBID:
-                helper.setSubject(String.format("You were outbid on %s.", request.title()));
-                helper.setText(
-                        String.format(
-                                "You were outbid on auction %s. Place a higher bid to compete. %n %s",
-                                request.title(),
-                                request.imageUrl()
-                        )
-                );
-                break;
-            case LOST:
-                helper.setSubject(String.format("You lost %s.", request.title()));
-                helper.setText(String.format("You lost auction %s. %n %s", request.title(), request.imageUrl()));
-                break;
-            case AUCTION_REMINDER:
-                helper.setSubject(String.format("Auction %s ends soon.", request.title()));
-                helper.setText(
-                        String.format(
-                                "Auction %s ends soon, dont miss out! %n %s",
-                                request.title(),
-                                request.imageUrl()
-                        )
-                );
-                break;
-            case ITEM_SOLD:
-                helper.setSubject(String.format("Item %s sold.", request.title()));
-                helper.setText(String.format("Item %s has sold. %n %s", request.title(), request.imageUrl()));
-                break;
-            case ITEM_NOT_SOLD:
-                helper.setSubject(String.format("Item %s not sold.", request.title()));
-                helper.setText(String.format(
-                        "Item %s has unfortunately not sold. %n %s",
-                        request.title(),
-                        request.imageUrl()
-                ));
-                break;
-            default:
-                log.warn("Unknown email type: {}", request.type());
-        }
+  @Async
+  public void sendEmail(EmailRequest request) {
+    try {
+      MimeMessage message = mailSender.createMimeMessage();
+      MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+      setEmailContent(helper, request);
+      helper.setTo(request.email());
+      mailSender.send(message);
+    } catch (MessagingException | MailException e) {
+      log.error("Failed to send email to {}: {}", request.email(), e.getMessage());
     }
+  }
+
+  public void setEmailContent(MimeMessageHelper helper, EmailRequest request)
+      throws MessagingException {
+    switch (request.type()) {
+      case WINNER:
+        helper.setSubject(String.format("Congratulations! You won %s.", request.title()));
+        helper.setText(
+            String.format(
+                "Congratulations! You won auction %s. Find payment and transport options under My Wins",
+                request.title()
+            )
+        );
+        break;
+      case OUTBID:
+        helper.setSubject(String.format("You were outbid on %s.", request.title()));
+        helper.setText(
+            String.format(
+                "You were outbid on auction %s. Place a higher bid to compete.",
+                request.title()
+            )
+        );
+        break;
+      case LOST:
+        helper.setSubject(String.format("You lost %s.", request.title()));
+        helper.setText(
+            String.format("You lost auction %s.", request.title()));
+        break;
+      case AUCTION_REMINDER:
+        helper.setSubject(String.format("Auction %s ends soon.", request.title()));
+        helper.setText(
+            String.format(
+                "Auction %s ends soon, dont miss out!",
+                request.title()
+            )
+        );
+        break;
+      case ITEM_SOLD:
+        helper.setSubject(String.format("Item %s sold.", request.title()));
+        helper.setText(
+            String.format("Item %s you have bidded on has unfortunately sold.", request.title()));
+        break;
+      case ITEM_NOT_SOLD:
+        helper.setSubject(String.format("Item %s not sold.", request.title()));
+        helper.setText(String.format(
+            "Item %s has unfortunately not sold.",
+            request.title()
+        ));
+        break;
+      default:
+        log.warn("Unknown email type: {}", request.type());
+    }
+  }
 }
